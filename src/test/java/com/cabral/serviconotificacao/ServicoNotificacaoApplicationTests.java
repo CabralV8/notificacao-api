3494b36359941ad07ca040b19c9a9cbdc0b05082
Valberton <valberton77@gmail.com>
2025-08-01 15:18:08 -0300

    commit inicial

---
+++ src/test/java/com/cabral/serviconotificacao/ServicoNotificacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cabral.serviconotificacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicoNotificacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
